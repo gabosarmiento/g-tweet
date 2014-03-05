59534700aeca4af8e8f80c0e3dec9b3ca7040b14
Gabosarmiento <gabrielsarmiento@gmail.com>
2014-03-05 03:52:55 -0500

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,60 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+    it "should have the h1 'Sample App'" do
+      visit '/static_pages/home'
+      page.should have_selector('h1', :text => 'G-Tweet')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/home'
+      page.should have_selector('title', 
+                        :text => "Ruby on Rails Tutorials G-Tweet | Home")
+    end
+  end
+
+  describe "Help page" do
+    it "should have the h1 'Help'" do
+      
+      visit '/static_pages/help'
+      page.should have_selector('h1', :text => 'Help')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/help'
+      page.should have_selector('title', 
+                        :text => "Ruby on Rails Tutorials G-Tweet | Help")
+    end
+  end
+
+  describe "About page" do
+    it "should have the h1 'About us'" do
+      
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'About Us')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/about'
+      page.should have_selector('title', 
+                        :text => "Ruby on Rails Tutorials G-Tweet | About Us")
+    end
+  end
+
+  describe "Contact page" do
+    it "should have the h1 'Contact'" do
+      
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', 
+                        :text => "Ruby on Rails Tutorials G-Tweet | Contact")
+    end
+  end
+
+end
