f9ceb75b6c4360b21bf9738c0d5a5f3892889dd3
Gabosarmiento <gabrielsarmiento@gmail.com>
2014-03-05 00:59:56 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GTweet::Application.config.secret_token = '2853f44f3a6bc5a9d1de690b34d09d290737ea34c85f9c4af09bf79b51de6faea7b41668c18214fb81e73510fe554efe2b8f1febe4b6feba91178336efb2af6f'
